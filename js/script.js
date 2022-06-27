727f8b24275296d7fa4eeabe55d3ab74da7ed39b
torone89 <torone89>
2022-06-27 17:00:23 +0200

    Aggiungo i bottoni

--- js/script.js
+++ js/script.js
@@ -28,6 +28,14 @@ console.log("JS")
 // MILESTONE 3
 // Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider, prendendo il posto della precedente.
 
+// MILESTONE 2
+// Adesso rimuoviamo tutto il markup statico e inseriamo le immagini dinamicamente servendoci dell'array fornito 
+// e un semplice ciclo for. Possiamo concatenare una stringa con un template literal oppure utilizzare gli altri
+//  metodi di manipolazione del DOM che abbiamo visto insieme. Al termine di questa fase ci ritroveremo con lo stesso slider,
+//   ma costruito dinamicamente attraverso
+
+
+
 // Mi recupero tutte le immagini per l'array
 
 const immagini = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg", "img/06.jpg"]
@@ -40,30 +48,26 @@ console.log(contenitore)
 
 // // Variabile per le immagini attive
 let activeIndex = 0
-immagini[activeIndex].classList.add("active")
-contenitore.innerHTML = `
-              <img src="${immagini[activeIndex]}" alt="immagine-${activeIndex}"`
+
+
+for (let i = 0; i < immagini.length; i++) {
 
 
 
-for (let activeIndex = 0; activeIndex < immagini.length; activeIndex++) {
 
+    contenitore.innerHTML += `
+          <img class="item" src="${immagini[i]}" alt="immagine-${activeIndex}">`
 
-
-    if (activeIndex === immagini.length) {
-        immagini[activeIndex].classList.remove("active")
-        contenitore.innerHTML = `
-          <img src="${immagini[activeIndex]}" alt="immagine-${activeIndex}"
-         
-              `
-
-    }
 
 }
-
+document.getElementsByClassName('item')[activeIndex].classList.add('active');
 
 console.log(immagini[activeIndex])
 
 
 
 console.log(activeIndex)
+
+
+// MILESTONE 3
+// Al click dell'utente sulle frecce, l'immagine attiva cambia e diventa visibile nello slider, prendendo il posto della precedente.
